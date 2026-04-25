db135d86a59f4240c015059a6ea25a346d38cb93
Randall Gann <rgann@gannsystems.com>
2026-04-24 20:52:51 -0500

    Initial Commit

---
+++ BuckFever/BuckFeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuckFeverApp.swift
+//  BuckFever
+//
+//  Created by Archon PAI on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct BuckFeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
